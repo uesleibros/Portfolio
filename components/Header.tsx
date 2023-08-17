673e47b41c4593443f809e0764d6b1dfbde4197a
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-08-17 20:33:04 -0300

    Added a hook Language Switcher and language system implemented.

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,36 +3,35 @@ import { Languages } from "lucide-react";
 import { poppins } from "@/fonts";
 import useLanguageSwitcher from "@/hooks/useLanguageSwitcher";
 import translations from "@/public/translations.json";
-import Translations from "@/types/translationsTypes";
 import Dropdown from "@/components/Dropdown";
 
 export default function Header() {
   const { language, changeLanguage, updateLanguage } = useLanguageSwitcher();
-  const Translations: Translations = translations;
 
-  console.log(Translations)  
-
+  let Translations = {};
   let aboutItems: Array<{ label: string | null | any, link?: string, labelType?: string | null }> = [];
   let technologiesItems: Array<{ label: string | null | any, link?: string, labelType?: string | null }> = [];
   let languagesItems: Array<{ label: string | null | any, link?: string, labelType?: string | null }> = [];
 
   if (language) {
+    Translations = translations[language.toLowerCase()];
+    const TranslationsAlt = translations.alts[language.toLowerCase()];
     aboutItems = [
-      { label: Translations[language.toLowerCase()].about.title as string, link: "#about" },
-      { label: Translations[language.toLowerCase()].about.s1.title as string, link: "#technical-expertise" },
-      { label: Translations[language.toLowerCase()].about.s2.title as string, link: "#problem-solver" },
-      { label: Translations[language.toLowerCase()].about.s3.title as string, link: "#lifelong-learner" }
+      { label: Translations.about.title as string, link: "#about" },
+      { label: Translations.about.s1.title as string, link: "#technical-expertise" },
+      { label: Translations.about.s2.title as string, link: "#problem-solver" },
+      { label: Translations.about.s3.title as string, link: "#lifelong-learner" }
     ];
 
     technologiesItems = [
-      { label: Translations[language.toLowerCase()].technologies.title as string, link: "#technologies" },
-      { label: Translations[language.toLowerCase()].technologies.s1.title as string, link: "#programming-languages" },
+      { label: Translations.technologies.title as string, link: "#technologies" },
+      { label: Translations.technologies.s1.title as string, link: "#programming-languages" },
       { label: "Frameworks", link: "#frameworks" }
     ];
 
     languagesItems = [
-      { label: Translations.alts[language.toLowerCase()].portuguese as string, labelType: "language-Portuguese" },
-      { label: Translations.alts[language.toLowerCase()].english as string, labelType: "language-English" }
+      { label: TranslationsAlt.portuguese as string, labelType: "language-Portuguese" },
+      { label: TranslationsAlt.english as string, labelType: "language-English" }
     ];
   }
 
@@ -57,24 +56,24 @@ export default function Header() {
               <>
                 <li>
                   <span className="transition duration-300 hover:text-gray-600">
-                    <Dropdown text={Translations[language.toLowerCase()].about.title} items={ aboutItems } />
+                    <Dropdown text={Translations.about.title} items={ aboutItems } />
                   </span>
                 </li>
                 <li>
                   <span className="transition duration-300 hover:text-gray-600">
-                    <Dropdown text={Translations[language.toLowerCase()].technologies.title} items={ technologiesItems } />
+                    <Dropdown text={Translations.technologies.title} items={ technologiesItems } />
                   </span>
                 </li>
                 <li>
-                  <a href="#projects" className="transition duration-300 hover:text-gray-600">{Translations[language.toLowerCase()].projects.title}</a>
+                  <a href="#projects" className="transition duration-300 hover:text-gray-600">{Translations.projects.title}</a>
                 </li>
                 <li>
-                  <a href="#contact" className="transition duration-300 hover:text-gray-600">{Translations[language.toLowerCase()].contacts.title}</a>
+                  <a href="#contact" className="transition duration-300 hover:text-gray-600">{Translations.contacts.title}</a>
                 </li>
                 <li className="px-6">
                   <span>
                     <Dropdown text=<Languages /> items={ languagesItems } />
-                    <small className="text-xs">({ Translations.alts[language.toLowerCase()][language.toLowerCase()] })</small>
+                    <small className="text-xs">({ Translations.alts })</small>
                   </span>
                 </li>
               </>
