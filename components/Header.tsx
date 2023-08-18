2377790f0f5a066933dcdd20800832242fefdfd8
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-08-17 21:49:04 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,10 +45,10 @@ export default function Header() {
 
   return (
     <header className="bg-gray-50 py-4 mb-20 w-full">
-      <div className="container flex flex-col justify-between items-center p-2">
+      <div className="container flex flex-col justify-center items-center p-2">
         <h1 className={`text-xl text-blue-500 mb-5 select-none font-bold ${poppins.className}`}>Ueslei Paim</h1>
         <nav>
-          <ul className="lg:flex sm:grid sm:grid-cols-1 sm:gap-4 sm:w-full lg:space-x-4 text-gray-400">
+          <ul className="lg:flex sm:grid sm:grid-cols-2 sm:gap-4 w-full lg:space-x-4 text-gray-400">
             {language && (
               <>
                 <li>
